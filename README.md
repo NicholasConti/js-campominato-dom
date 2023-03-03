d3d1ade8b18cfac2d2f85db165d62f9a94919680
NicholasConti <conti94@live.it>
2023-03-03 15:02:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+STEPS
+- creo un array vuoto
+- lo riempio con 16 numeri casuali controllando che non si ripetano
+  e con valori possibili in base alla difficolta scelta
+
+- creo una classe nel css per il bg rosso
+- all evento di pressione della casella:
+    - se il numero e nell array assegno la classe del bg rosso 
+    -se non e compreso assegno solo il bg blu
+
+- creo un contatore per tenere contro delle caselle selezionate che non erano bombe
+- per terminare la partita controllo se un elemento ha la classe del bg rosso oppure se il contatore raggiunge il valore del numero di caselle-16
+
+- comunico il punteggio del contatore
---
+++ js/mian.js
@@ -0,0 +1,63 @@
+"use strict";
+
+//------------------
+//FUNCTIONS
+//------------------
+function myCreateEl(tagEl, classEl, numCell, level){
+    const element = document.createElement(tagEl);  //creazioen div class= cell
+    element.classList.add(classEl);
+    element.classList.add(level);
+    element.innerText = numCell;
+    //CAMBIO COLORE
+    element.addEventListener('click',
+        function(){
+            element.classList.add('colorbg');
+            console.log(numCell);
+        }
+    )
+    return element;
+}
+
+function appendElement(containerEl, sonEl){
+    containerEl.append(sonEl);
+}
+
+
+//------------------
+//MAIN
+//------------------
+const cellBoard = document.querySelector('.board');
+
+const button = document.getElementById("start");
+
+const select = document.getElementById("levels");
+
+//creazione tabella
+button.addEventListener('click',
+    function(){
+        // variabili per cambiare numero caselle e assegnare classe con dimsensioni diverse
+        let classLV;
+        let numberOfCells;
+        switch (select.value) {
+            case 'lv1':
+                classLV = 'lv1';
+                numberOfCells = 100;
+                break;
+            case 'lv2':
+                classLV = 'lv2';
+                numberOfCells = 81;
+                break;
+            case 'lv3':
+                classLV = 'lv3';
+                numberOfCells = 49;
+                break;
+        }
+        cellBoard.innerHTML = ''; // reset board
+        //riempimento board
+        for(let i = 1; i <= numberOfCells; i++){
+            const createdElement = myCreateEl('div', 'cell', i, classLV);
+            appendElement(cellBoard, createdElement);
+        }
+
+    }
+);
